6bce094eb7a88766c8eb5c4e76e5b672a7b68401
Haley Someone <hbarlar1@gmail.com>
2018-07-23 23:29:55 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |
+  batch_badge_creator(attendees).each do |i|
+    
   assign_rooms
 end
     
