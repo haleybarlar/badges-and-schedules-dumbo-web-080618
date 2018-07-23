3b4eadffaaf11237f28ff395de1fd48e095f338d
Haley Someone <hbarlar1@gmail.com>
2018-07-23 23:29:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,10 +11,10 @@ def batch_badge_creator(namesArray)
 end
 
 def assign_rooms(attendees)
-  rooms = 0
+  room = 0
   attendees.collect do |name|
-    rooms += 1
-    "Hello, #{name}! You'll be assigned to room #{rooms}!"
+    room += 1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
   end
 end
 
