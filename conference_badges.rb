9a5eb078c8f8833fac3503e05d29ae3f1fb21e48
Haley Someone <hbarlar1@gmail.com>
2018-07-23 23:22:24 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(namesArray)
+  newArray = []
+  namesArray.each do |i|
+    newArray.push("Hello, my name is #{i}.")
+  end
+  return newArray
+end
+
+def assign_rooms(array)
+  rooms = (1...7)
+  newArray = []
+  array.each do |i|
+    newArray.push("Hello, #{i}! You'll be assigned to room #{rooms[i]}!")
+  end
+  return newArray
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees)
+  assign_rooms
+end
+    
+    
