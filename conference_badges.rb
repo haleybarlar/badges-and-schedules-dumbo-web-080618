b1dc0035612a59577c55b07c645dae9e116f34f9
Haley Someone <hbarlar1@gmail.com>
2018-07-23 23:24:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,12 +12,7 @@ end
 
 def assign_rooms(attendees)
   rooms = 0
-  newArray = []
-  array.each do |i|
-    newArray.push("Hello, #{i}! You'll be assigned to room #{rooms[i]}!")
-  end
-  return newArray
-end
+  
 
 def printer(attendees)
   batch_badge_creator(attendees)
